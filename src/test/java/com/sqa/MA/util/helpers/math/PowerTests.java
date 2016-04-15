d6350f7442b5a98f458582c5937b93c7664a79dc
mikias ayalew <ammrdf@yahoo.com>
2016-04-14 21:20:11 -0700

    TestNG

--- src/test/java/com/sqa/MA/util/helpers/math/PowerTests.java
+++ src/test/java/com/sqa/MA/util/helpers/math/PowerTests.java
@@ -10,12 +10,10 @@
 
 package com.sqa.MA.util.helpers.math;
 
-import org.junit.Assert;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import com.sqa.MA.util.helpers.MyMath;
 
@@ -31,10 +29,10 @@ import com.sqa.MA.util.helpers.MyMath;
  * @since 1.0
  *
  */
-@RunWith(Parameterized.class)
+
 public class PowerTests {
 
-	@Parameters
+	@DataProvider
 	public static Object[][] getData() {
 		Object[][] data = { { 2.0, 2.0, 4.0 }, { 5.0, 2.0, 25.0 }, { 4.0, 3.0, 64.0 }, { 1.0, 0.0, 1.0 },
 				{ 3.0, 1.0, 3.0 }, { 2.0, 1.5, 4 } };
@@ -42,70 +40,14 @@ public class PowerTests {
 
 	}
 
-	private double expectedResult;
-	private double num;
-	private double power;
-
-	public PowerTests(double num, double power, double expextedResult) {
-		super();
-		this.expectedResult = expextedResult;
-		this.num = num;
-		this.power = power;
-	}
-
-	@Test
-	@Ignore
-	public void testDivideNumbers() {
-		double result;
-		double x = 8;
-		double y = 2;
-		result = MyMath.divideNumbers(x, y);
-		System.out.println(String.format("The result of divistion %.0f and %.0f is %.0f ", x, y, result));
-
-	}
-
-	@Test
-	@Ignore
-	public void testMultNumbers() {
-		double result;
-		double x = 2;
-		double y = 4;
-		double z = 3;
-		result = MyMath.multNumbers(x, y, z);
-		System.out
-				.println(String.format("The result of multiplication %.0f and %.0f and %.0f is %.0f", x, y, z, result));
-
-	}
-
-	@Test
-	@Ignore
-	public void testMyMathClass() {
-		double result;
-		double addNumber = 10;
-		double subNumber = 3;
-		double multNumber = 4;
-
-		MyMath myMath = new MyMath();
-		myMath.add(addNumber);
-		myMath.sub(subNumber);
-		myMath.mult(multNumber);
-		result = myMath.getTotal();
-		System.out.println(
-				String.format("The result of 0 and adding " + "%.0f and than" + " subtracting" + " %.0f is %.0f",
-						addNumber, subNumber, multNumber, result));
-
-	}
-
-	@Test
-	public void testPowerNumbers() {
+	@Test(dataProvider = "getData")
+	public void testPowerNumbers(double num, double power, double expextedResult) {
 		double actualResult;
-		double expectedResult = this.expectedResult;
-		double num = this.num;
-		double power = this.power;
 		actualResult = MyMath.powerNumber(num, power);
 		String message = String.format("The result of %.0f to the power of %.0f is %.0f", num, power, actualResult);
 		System.out.println(message);
-		Assert.assertEquals(message, expectedResult, actualResult, 0);
+		assertEquals(actualResult, expextedResult, 0, message);
+
 	}
 
 }
